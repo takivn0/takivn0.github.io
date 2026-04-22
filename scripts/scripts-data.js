09e3f8b214dcf0be4c6dcadb8856940371051089
takivn0 <takiclone54@gmail.com>
2026-04-22 18:59:33 +0700

    Add files via upload

---
+++ scripts/scripts-data.js
@@ -0,0 +1,33 @@
+window.scripts = {
+  "pshade": {
+    title: "Pshade",
+    image: "assets/pshade.png",
+    desc: "Thêm shader vào Roblox",
+    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/pshade-ultimate/refs/heads/main/src/cd.lua"))()'
+  },
+  "ink-game": {
+    title: "Ink Game: Ringta",
+    image: "assets/ink-game.png",
+    desc: "Trở thành nhân vật chính không khó (trừ khi xài ngu)",
+    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/wefwef127382/inkgames.github.io/refs/heads/main/ringta.lua"))()'
+  },
+  "piggy": {
+    title: "Piggy: Star Hack Hub",
+    image: "assets/piggy.png",
+    desc: "Script Piggy giúp mấy thằng bị skill issue",
+    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/StarHackScripts/StarHack-Hub-Deepstar-Hub/refs/heads/main/StarHackHub_PiggyV2release"))()'
+  },
+  "doors": {
+    title: "Doors: Blackking",
+    image: "assets/doors.png",
+    desc: "Blackking là script khá ngon cho Doors",
+    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/GeorgeRoblox/BlackKingKeylessNew/refs/heads/main/New.Lua"))()'
+  },
+  "nameless-admin": {
+    title: "Nameless Admin",
+    image: "assets/NAnew.png",
+    desc: "Nameless Admin là bản cải tiến của Infinite Yield (Chắc thế).",
+    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/Nameless-Admin/main/Source.lua"))();'
+  }
+};
+// Thêm script khác ở đây
